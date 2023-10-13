56b618f9a157995420d4b44a2b83e9a5c1a81c3b
ravindu0823 <guestpc87@gmail.com>
2023-10-14 00:11:51 +0530

    fix: fix footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'HACKOVATE',
-  description: 'HACKOVATE 2023 Organized by FOSS Community of NSBM',
-}
+  title: "HACKOVATE",
+  description: "HACKOVATE 2023 Organized by FOSS Community of NSBM",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -2,56 +2,58 @@ import React from "react";
 import Image from "next/image";
 
 const AboutSection = () => {
-    return (
-        <>
-            <div className='flex' id="About">
-                <div className='left-0 mx-6 pt-28'>
-                    <Image
-                        className='hidden md:block'
-                        src='/assets/e2.png'
-                        alt='symbol-2'
-                        width={500}
-                        height={500}
-                    />
-                </div>
-                <div className="px-2">
-                    <p className="font-bold mt-20 mb-8" style={{ fontSize: "32px" }}>ABOUT EVENT</p>
-                    <p className="font-bold" style={{ fontSize: "18px" }}>
-                        Forem ipsum dolor sit amet, consectetur adipiscing elit.Forem ipsum
-                        dolor sit amet, consectetur adipiscing elit.Forem ipsum dolor sit
-                        amet, consectetur adipiscing elit.Forem ipsum dolor sit amet,
-                        consectetur adipiscing elit.Forem ipsum dolor sit amet, consectetur
-                        adipiscing elit.
-                    </p>
-                    <br />
-                    <p className="font-bold" style={{ fontSize: "18px" }}>
-                        Forem ipsum dolor sit amet, consectetur adipiscing elit.Forem ipsum
-                        dolor sit amet, consectetur adipiscing elit.Forem ipsum dolor sit
-                        amet, consectetur adipiscing elit.Forem ipsum dolor sit amet,
-                        consectetur adipiscing elit.Forem ipsum dolor sit amet, consectetur
-                        adipiscing elit.Forem ipsum dolor sit amet, consectetur adipiscing
-                        elit.Forem ipsum dolor sit amet, consectetur adipiscing elit.Forem
-                        ipsum dolor sit amet, consectetur adipiscing elit.Forem ipsum dolor
-                        sit amet, consectetur adipiscing elit.Forem ipsum dolor sit amet,
-                        consectetur adipiscing elit.Forem ipsum dolor sit amet, consectetur
-                        adipiscing elit.Forem ipsum dolor sit amet, consectetur adipiscing
-                        elit.Forem ipsum dolor sit amet, consectetur adipiscing elit.Forem
-                        ipsum dolor sit amet, consectetur adipiscing elit.Forem ipsum dolor
-                        sit amet, consectetur adipiscing elit.
-                    </p>
-                </div>
-                <div className="right-0 pt-48 me-8">
-                    <Image
-                        className='hidden md:block'
-                        src='/assets/e1.png'
-                        alt='symbol-1'
-                        width={500}
-                        height={500}
-                    />
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex" id="About">
+        <div className="left-0 mx-6 pt-28">
+          <Image
+            className="hidden md:block"
+            src="/assets/e2.png"
+            alt="symbol-2"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className="px-2">
+          <p className="font-bold mt-20 mb-8" style={{ fontSize: "32px" }}>
+            ABOUT EVENT
+          </p>
+          <p className="font-bold" style={{ fontSize: "18px" }}>
+            Forem ipsum dolor sit amet, consectetur adipiscing elit.Forem ipsum
+            dolor sit amet, consectetur adipiscing elit.Forem ipsum dolor sit
+            amet, consectetur adipiscing elit.Forem ipsum dolor sit amet,
+            consectetur adipiscing elit.Forem ipsum dolor sit amet, consectetur
+            adipiscing elit.
+          </p>
+          <br />
+          <p className="font-bold" style={{ fontSize: "18px" }}>
+            Forem ipsum dolor sit amet, consectetur adipiscing elit.Forem ipsum
+            dolor sit amet, consectetur adipiscing elit.Forem ipsum dolor sit
+            amet, consectetur adipiscing elit.Forem ipsum dolor sit amet,
+            consectetur adipiscing elit.Forem ipsum dolor sit amet, consectetur
+            adipiscing elit.Forem ipsum dolor sit amet, consectetur adipiscing
+            elit.Forem ipsum dolor sit amet, consectetur adipiscing elit.Forem
+            ipsum dolor sit amet, consectetur adipiscing elit.Forem ipsum dolor
+            sit amet, consectetur adipiscing elit.Forem ipsum dolor sit amet,
+            consectetur adipiscing elit.Forem ipsum dolor sit amet, consectetur
+            adipiscing elit.Forem ipsum dolor sit amet, consectetur adipiscing
+            elit.Forem ipsum dolor sit amet, consectetur adipiscing elit.Forem
+            ipsum dolor sit amet, consectetur adipiscing elit.Forem ipsum dolor
+            sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+        <div className="right-0 pt-48 me-8">
+          <Image
+            className="hidden md:block"
+            src="/assets/e1.png"
+            alt="symbol-1"
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AboutSection;
