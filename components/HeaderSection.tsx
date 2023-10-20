7627b3d4272189aa484d57fb0ffc1e85959d5b29
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2023-10-20 19:10:48 +0530

    fix: countdown light belt

--- components/HeaderSection.tsx
+++ components/HeaderSection.tsx
@@ -128,7 +128,7 @@ const HeaderSection = () => {
                                 backgroundColor: countdown.days >= 2 ? '#EC4237' : '',
                             }}></div>
                             <div className="Herostyles__StyledCountdownLoadingItem-sc-ofav2u-12 jSlIIA" style={{
-                                backgroundColor: (countdown.minutes >= 1 && countdown.days >= 0) ? '#EC4237' : '',
+                                backgroundColor: (countdown.minutes == 0 && countdown.hours == 0 && countdown.days == 0 ) ? '' : '#EC4237',
                             }}></div>
 
                         </div>
