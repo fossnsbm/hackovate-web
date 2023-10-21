eb304c1cde7cbf9243c89ec910ce187250758e92
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2023-10-21 12:22:47 +0530

    fix: redirect location

--- app/events/hackovate/register/page.tsx
+++ app/events/hackovate/register/page.tsx
@@ -11,7 +11,7 @@ const RegisterPage = () => {
   //redirect home
   const router = useRouter();
   useEffect(() => {
-    router.push("/");
+    router.push("/events/register");
   }, []);
   return (
     <>
