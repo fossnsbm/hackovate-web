684c63c795e279dc692e8f40bbee939446c40758
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2023-10-23 00:38:23 +0530

    feat: added achievements chart

--- app/events/hackovate/start-hacking/page.tsx
+++ app/events/hackovate/start-hacking/page.tsx
@@ -5,6 +5,7 @@ import NavBar from "@/components/NavBar";
 import Guidelines from "@/components/comp-hack/Guidelines";
 import Issues from "@/components/comp-hack/Issues";
 import Scenario from "@/components/comp-hack/Scenario";
+import GiftsChart from "@/components/comp-hack/GiftsChart";
 
 
 const startHacking = () => {
@@ -14,6 +15,12 @@ const startHacking = () => {
             <Guidelines />
             <Scenario />
             <Issues />
+            <GiftsChart />
+            <div className="ms-10 md:ms-36 pb-0 md:pb-6 pt-10 md:pt-28 animate-pulse italic">
+                <p className="text-[#F0D268] text-gray text-2xl md:text-4xl lg:text-5xl font-extrabold uppercase">
+                    Happy Hacking!
+                </p>
+            </div>
             <LongFooter />
         </>
     );
